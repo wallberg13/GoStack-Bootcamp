0c5b69fa48305622b1162298ce7d87a2319a49bd
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-04-20 00:52:08 -0300

    Adicionado rotas para obtencao e upload de arquivos
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel02/02-IniciandoBackendDoApp/GoBarberBackend/src/routes/users.routes.ts
+++ Nivel02/02-IniciandoBackendDoApp/GoBarberBackend/src/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
 import CreateUserService from "../services/CreateUserService";
-
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
+
 /**
  * Repositoriese
  * Services
@@ -29,8 +33,29 @@ userRouter.post("/", async (request, response) => {
 /**
  * Alterando somente uma informação.
  */
-userRouter.patch("/avatar", ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
+
+userRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      // const { user_id: id } = request.user;
+
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default userRouter;
--- Nivel02/02-IniciandoBackendDoApp/GoBarberBackend/src/server.ts
+++ Nivel02/02-IniciandoBackendDoApp/GoBarberBackend/src/server.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import express from "express";
 import routes from "./routes";
 
+import uploadConfig from "./config/upload";
+
 /**
  * Para utilizar o TypeORM, basta iniciar a aplicação, e importar a mesma,
  * que já saí tudo funcionando (já entra fudendo!!);
@@ -11,6 +13,9 @@ import "./database";
 const app = express();
 
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.listen(3333, () => {
