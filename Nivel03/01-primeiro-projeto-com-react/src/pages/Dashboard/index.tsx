e64ac480d6df287495e8a68b17007d9c308d64ed
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-04-26 20:38:10 -0300

    Conectando com a API do Git
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/index.tsx
+++ Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 /**
  * Todo arquivo CSS ele se torna sendo global na aplicação, assim,
  * possuindo a necessidade de limitar cada CSS em um componente.
@@ -7,6 +8,19 @@ import { FiChevronRight } from "react-icons/fi";
  */
 import logoImg from "../../assets/logo.svg";
 import { Title, Form, Repositories } from "./styles";
+
+/**
+ * Não precisamos colocar a Tipagem de tudo o que o meu repositório vai ter.
+ */
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 /**
  * Por que utilizar no formato de constante:
  * - Pq é mais simples definir a tipagem do componente.
@@ -14,62 +28,54 @@ import { Title, Form, Repositories } from "./styles";
  * O Tipo React.FC é de: React Function
  *
  * Semânticamente, algo clicável no HTML, ou é uma ancora (a), ou é um botão.
+ *
+ * wallberg13/GoStack-Bootcamp
  */
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setNewRepo("");
+    setRepositories([...repositories, repository])
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-            alt="Wall Berg Morais"
-          />
-          <div>
-            <strong>Maratona Linux Utils</strong>
-            <p>Pacote com todos os útils das Maratona Linux</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-            alt="Wall Berg Morais"
-          />
-          <div>
-            <strong>Maratona Linux Utils</strong>
-            <p>Pacote com todos os útils das Maratona Linux</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-            alt="Wall Berg Morais"
-          />
-          <div>
-            <strong>Maratona Linux Utils</strong>
-            <p>Pacote com todos os útils das Maratona Linux</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-            alt="Wall Berg Morais"
-          />
-          <div>
-            <strong>Maratona Linux Utils</strong>
-            <p>Pacote com todos os útils das Maratona Linux</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+
       </Repositories>
     </>
   );
