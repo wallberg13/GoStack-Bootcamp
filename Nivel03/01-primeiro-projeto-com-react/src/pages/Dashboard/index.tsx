20fb16a51d4a178093317b66b96e9f0514455d06
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-04-26 16:56:18 -0300

    Construido o Layout do Dashboard
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/index.tsx
+++ Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/index.tsx
@@ -1,20 +1,78 @@
 import React from "react";
-
+import { FiChevronRight } from "react-icons/fi";
 /**
  * Todo arquivo CSS ele se torna sendo global na aplicação, assim,
  * possuindo a necessidade de limitar cada CSS em um componente.
  * Para isso, é necessário criar umas Divs
  */
-
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+import { Title, Form, Repositories } from "./styles";
 /**
  * Por que utilizar no formato de constante:
  * - Pq é mais simples definir a tipagem do componente.
  *
  * O Tipo React.FC é de: React Function
+ *
+ * Semânticamente, algo clicável no HTML, ou é uma ancora (a), ou é um botão.
  */
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
+            alt="Wall Berg Morais"
+          />
+          <div>
+            <strong>Maratona Linux Utils</strong>
+            <p>Pacote com todos os útils das Maratona Linux</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
+            alt="Wall Berg Morais"
+          />
+          <div>
+            <strong>Maratona Linux Utils</strong>
+            <p>Pacote com todos os útils das Maratona Linux</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
+            alt="Wall Berg Morais"
+          />
+          <div>
+            <strong>Maratona Linux Utils</strong>
+            <p>Pacote com todos os útils das Maratona Linux</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/8339296?s=460&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
+            alt="Wall Berg Morais"
+          />
+          <div>
+            <strong>Maratona Linux Utils</strong>
+            <p>Pacote com todos os útils das Maratona Linux</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/styles.ts
+++ Nivel03/01-primeiro-projeto-com-react/src/pages/Dashboard/styles.ts
@@ -1,9 +1,114 @@
 import styled from "styled-components";
-
+import { shade } from "polished";
 //
 // Template Literals
 //
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+/**
+ * Encadeamentos de CSS.
+ *
+ * SASS, LESS: Préprocessadores de CSS.
+ */
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    /* Estilizando as propriedades do proprio componente (nao sei se é isso que entendi) */
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    /** Outro Hack - Border RADIUS igual à 50% do heigh, forma uma parte da circuferência */
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    /* Se referênciando o proprio componente */
+    &:hover {
+      background: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  margin-bottom: 100px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    /** Por pardrão, a ancora vem com o display inline do CSS. */
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    /* Colocando um margin top a partir do segundo elemento (e nao do primeiro, pois nao faz sentido);
+     * & representa o proprio componente. O a representa componentes do tipo. Então não faz sentido colocar & + &,
+     * pois teoricamente, eu não sou precedido de eu mesmo.
+     */
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      /* Move 10 pixels para algum lado */
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%; /** Assim, a imagem fica totalmente arredondada, com 50% de border radius */
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      /* Pega todo o espaço disponível da esquerda e joga para o lado*/
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
