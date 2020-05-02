a7b5e46314a9812d4beca4c8837434b9cd3d185e
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-05-01 23:29:09 -0300

    Add login page.
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel03/02-gobarber-web/src/App.tsx
+++ Nivel03/02-gobarber-web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import SignIn from "./pages/SignIn";
+import GlobalStyle from "./styles/global";
 
-const App: React.FC = () => <h1>Caralhos de Aza</h1>;
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
---
+++ Nivel03/02-gobarber-web/src/pages/SignIn/styles.ts
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import { shade } from "polished";
+import signInBackgroundImg from "../../assets/sign-in-background.png";
+
+export const Container = styled.div`
+  /** Forçar que o container possua 100% do View Port Height, ou seja,
+      100% do tamanho da tela que está sendo exibida.
+   */
+  height: 100vh;
+
+  /*
+    Display Flex faz com que o background e o conteúdo fique um no lado do outro.
+    O stretch vai garantir que os dois tomem o total espaço da tela, ele estica o máximo que poder.
+   */
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /*
+    Place-Concent diz que tudo e qualquer coisa
+    que seja colocada no componente, vai ficar no centro
+  */
+  place-content: center;
+  width: 100%;
+  /* Nunca passa dessa quantidade de pixels */
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      padding: 16px;
+      width: 100%;
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, "#ff9000")};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, "#f4ede8")};
+      }
+    }
+  }
+
+  /* Estilizando somente as ancoras que estão vindo diretamente do meu content, e não mais
+     um nivel para dentro.
+   */
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, "#ff9000")};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  /* Faz com que o componente ocupe todo o espaço disponivel para o mesmo, ou seja, é flexível. */
+  flex: 1;
+
+  /*
+    Adicionando a imagem de fundo, para não repetir e fica centralizada
+   */
+  background: url(${signInBackgroundImg}) no-repeat center;
+  /* O cover faz com que o background ocupe toda a parte do container, mesmo que ele nao ocupe todo o espaço,
+     e é um fato muito importante para a responsividade da aplicação.
+   */
+  background-size: cover;
+`;
