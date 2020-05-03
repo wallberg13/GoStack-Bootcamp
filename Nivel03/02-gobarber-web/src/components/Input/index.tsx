af34ff61195472259cdf53678fb827779883ed99
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-05-03 00:33:44 -0300

    Adicionando usabilidade do input
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel03/02-gobarber-web/src/components/Input/index.tsx
+++ Nivel03/02-gobarber-web/src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from "react";
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback
+} from "react";
 import { IconBaseProps } from "react-icons";
 import { useField } from "@unform/core";
 import { Container } from "./styles";
@@ -11,11 +17,30 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 /**
  * Renomenado o icon, pois o React não aceita componentes com a primeira letra
  * minuscula.
+ *
+ * useCallback: é uma forma de criar funções dentro do componente sem que a mesma
+ * seja "recriada" em memória toda vez que o componente é atualizado. Ou seja, o componente
+ * passa a ter memória. Possui os mesmos parâmetros do useEffect, com diferença que
+ * esta mesma é criada para funções. No segundo parâmetro, ele diz que toda vez que
+ * uma váriavél for alterada, a função precisa ser recriada.
+ *
  */
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   // useRef faz com que possamos fazer manipulações em cada elemento isolado (ou componentes)
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
   const { fieldName, defaultValue, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
 
   useEffect(() => {
     // Path: caminho para obter o valor do Input.
@@ -27,9 +52,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container>
+    <Container isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
-      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
     </Container>
   );
 };
