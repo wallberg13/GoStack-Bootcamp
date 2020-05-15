c95c49760c98adaccf588bc60938c0a81221afd9
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-05-15 17:05:18 -0300

    Finalizando SignIn
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel03/03-gobarber-mobile/src/pages/SignIn/index.tsx
+++ Nivel03/03-gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,20 +1,72 @@
 import React from "react";
-import { Image } from "react-native";
-import { Container, Title } from "./styles";
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import logoImg from "../../assets/logo.png";
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from "./styles";
 
 const SignIn: React.FC = () => {
+  8;
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Logon</Title>
-      <Input name="Caralho Lek" icon="mail" placeholder="E-mail" />
-      <Input name="Caralho Lek" icon="lock" placeholder="Senha" />
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+    <>
+      {/*
+         KeyboardAvoidingView -> Comportamento desejado para quando o teclado aparecer em tela
+         e o mesmo permitir que quando o usuário dá um toque fora da tela, ele seja fechado.
+
+         -> O comportamento no Android de quando o teclado é aberto, o mesmo leva todos os componentes
+            em tela para cima, o ideal, é escutar um evento de quando o teclado é aberto ou não, e caso afirmativo,
+            não renderizar tal componente.
+        */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }} // Flex 1 para ocupar toda a janela
+        // Platform é uma lib do React-Native que indentifica em qual plataforma estamos desenvolvendo.
+        //
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled // Mantem habilitado por padrao
+      >
+        {/* ScrollView é para quando o conteúdo em tela extrapolar, o mesmo ter a possibilidade de está dentro do scroll*/}
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu Logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {/* Botão para criar a conta fora do container pois ele possui um position absolute. */}
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma Conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
