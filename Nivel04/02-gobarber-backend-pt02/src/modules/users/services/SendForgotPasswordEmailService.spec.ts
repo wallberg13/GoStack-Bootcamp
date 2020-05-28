73f9d3c168b2920b4bb856a9fc75237189609553
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-05-28 00:24:20 -0300

    Iniciando a recuperaçao de senha
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

---
+++ Nivel04/02-gobarber-backend-pt02/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,37 @@
+// import AppError from "@shared/errors/AppError";
+
+import FakeMailProvider from "@shared/container/providers/MailProvider/fakes/FakeMailProvider";
+import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
+
+import SendForgotPasswordEmailService from "./SendForgotPasswordEmailService";
+
+describe("SendForgotPasswordEmail", () => {
+  it("should be able to recover the password using the email", async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, "sendMail");
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider
+    );
+
+    await fakeUsersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456"
+    });
+
+    await sendForgotPasswordEmailService.execute({
+      email: "johndoe@example.com"
+    });
+
+    // qual resultado estou esperando?
+    expect(sendMail).toBeCalled();
+  });
+});
+
+// RED
+// GREEN - Não da melhor forma possível
+// REFACTOR
