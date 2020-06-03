3b7591ccfa739449bcc4e96feb0cc09a043df5e6
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-03 00:58:08 -0300

    Add EmailTemplate
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/02-gobarber-backend-pt02/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ Nivel04/02-gobarber-backend-pt02/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,13 +1,21 @@
 import nodemailer, { Transporter } from "nodemailer";
+import { injectable, inject } from "tsyringe";
+import IMailTemplateProvider from "@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider";
 import IMailProvider from "../models/IMailProvider";
+import ISendMailDTO from "../dtos/ISendMailDTO";
 
 // Nosso provider sempre é executado uma unica vez.
 // Inicialmente, para utilizar o EtherealMailProvider, inicialmente
 // pricesamos criar uuma conta gratuita.
+
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject("MailTemplateProvider")
+    private mailTemplateProvider: IMailTemplateProvider
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -23,12 +31,20 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    subject,
+    from,
+    template
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: "Equipe GoBarber <equipe@gobarber.com.br>",
-      to,
-      subject: "Recuperação de Senha",
-      text: body
+      from: {
+        name: from?.name || "Equipe GoBarber",
+        address: from?.email || "equipe@gobarber.com.br"
+      },
+      to: { name: to.name, address: to.email },
+      subject,
+      html: await this.mailTemplateProvider.parse(template)
     });
     console.log("Message sent: %s", message.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
--- Nivel04/02-gobarber-backend-pt02/src/shared/container/providers/index.ts
+++ Nivel04/02-gobarber-backend-pt02/src/shared/container/providers/index.ts
@@ -6,12 +6,22 @@ import DiskStorageProvider from "./StorageProvider/implementations/DiskStoragePr
 import IMailProvider from "./MailProvider/models/IMailProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 
+import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
+import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
   DiskStorageProvider
 );
 
+// Como o MailProvider depende do MailTemplateProvider, o mesmo deve vim após
+// ao MailTemplateProvider para obedecer a ordem das dependências.
+container.registerSingleton<IMailTemplateProvider>(
+  "MailTemplateProvider",
+  HandlebarsMailTemplateProvider
+);
+
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  new EtherealMailProvider()
+  container.resolve(EtherealMailProvider)
 );
