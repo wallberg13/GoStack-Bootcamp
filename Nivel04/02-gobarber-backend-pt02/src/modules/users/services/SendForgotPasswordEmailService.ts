9dc56ca9d8d63e520c8957f4f6dff57b4f2f78c2
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-04 18:35:14 -0300

    Add template engine in forgot password send mail
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/02-gobarber-backend-pt02/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ Nivel04/02-gobarber-backend-pt02/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 // import AppError from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+import path from "path";
 
 // import User from "../infra/typeorm/entities/User";
 import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider";
@@ -34,14 +35,21 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      "..",
+      "views",
+      "forgot_password.hbs"
+    );
+
     await this.mailProvider.sendMail({
       to: { name: user.name, email: user.email },
       subject: "[GoBarber] Recuperação de senha",
       template: {
-        template: "Olá, {{name}} - Seu Token {{token}}",
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token
+          link: `http://localhost:3000/reset_password?token=${token}`
         }
       }
     });
