f99d0c3034108f3a2da6c49627c40bd9f765e572
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-07 11:50:51 -0300

    Add listAllProviders
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

---
+++ Nivel04/02-gobarber-backend-pt02/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
+import ProvidersController from "../controllers/ProvidersController";
+
+const providersRouter = Router();
+const providersController = new ProvidersController();
+
+providersRouter.use(ensureAuthenticated);
+
+// Rota completa: http://localhost:3333/providers/
+providersRouter.get("/", providersController.index);
+
+export default providersRouter;
--- Nivel04/02-gobarber-backend-pt02/src/modules/users/repositories/IUsersRepository.ts
+++ Nivel04/02-gobarber-backend-pt02/src/modules/users/repositories/IUsersRepository.ts
@@ -1,5 +1,6 @@
 import User from "../infra/typeorm/entities/User";
 import ICreateUserDTO from "../dtos/ICreateUserDTO";
+import IFindAllProvidersDTO from "../dtos/IFindAllProvidersDTO";
 
 // Encontrar por email - FindByEmail
 // Encontrar por id - FindByID
@@ -7,6 +8,7 @@ import ICreateUserDTO from "../dtos/ICreateUserDTO";
 // save
 
 export default interface IUsersRepository {
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
