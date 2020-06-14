ae5927233d0a2542ee09568d946ca902d1d008fc
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-14 20:11:22 -0300

    Add list provider schedule
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/03-gobarber-backend-pt03/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Nivel04/03-gobarber-backend-pt03/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,22 +2,20 @@ import { Router } from "express";
 
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
 import AppointmentsController from "../controllers/AppointmentsController";
+import ProviderAppointmentsController from "../controllers/ProviderAppointmentsController";
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 // Controllers serve para abstrair a logica das nossas rotas.
 
 // Adicionando o Middleware em todas as rotas do appointments
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get("/", async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
 // Rota completa: http://localhost:3333/appointments/
 appointmentsRouter.post("/", appointmentsController.create);
+
+appointmentsRouter.get("/me", providerAppointmentsController.index);
 
 export default appointmentsRouter;
