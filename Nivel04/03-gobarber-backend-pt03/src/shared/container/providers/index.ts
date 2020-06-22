a1d2bcf4beee1a272e285bc05fe228cca002f74d
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-21 23:37:51 -0300

    Update Providers folders topology
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/index.ts
+++ Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/index.ts
@@ -1,34 +1,3 @@
-import { container } from "tsyringe";
-import mailConfig from "@config/mail";
-
-import IStorageProvider from "./StorageProvider/models/IStorageProvider";
-import DiskStorageProvider from "./StorageProvider/implementations/DiskStorageProvider";
-
-import IMailProvider from "./MailProvider/models/IMailProvider";
-import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
-import SESMailProvider from "./MailProvider/implementations/SESMailProvider";
-
-import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
-import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
-
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  DiskStorageProvider
-);
-
-// Como o MailProvider depende do MailTemplateProvider, o mesmo deve vim após
-// ao MailTemplateProvider para obedecer a ordem das dependências.
-container.registerSingleton<IMailTemplateProvider>(
-  "MailTemplateProvider",
-  HandlebarsMailTemplateProvider
-);
-
-const mailDriver = {
-  ethereal: container.resolve(EtherealMailProvider),
-  ses: container.resolve(SESMailProvider)
-};
-
-container.registerInstance<IMailProvider>(
-  "MailProvider",
-  mailDriver[mailConfig.driver]
-);
+import "./StorageProvider";
+import "./MailTemplateProvider";
+import "./MailProvider";
