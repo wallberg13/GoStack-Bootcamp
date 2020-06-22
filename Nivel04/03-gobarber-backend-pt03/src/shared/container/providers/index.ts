cdf82357fc17f09c78d10bb7dddd3d8b488a7bc8
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-21 23:06:15 -0300

    Add SESMailProvider with ingration to AmazonSES
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/index.ts
+++ Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from "tsyringe";
+import mailConfig from "@config/mail";
 
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import DiskStorageProvider from "./StorageProvider/implementations/DiskStorageProvider";
 
 import IMailProvider from "./MailProvider/models/IMailProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
+import SESMailProvider from "./MailProvider/implementations/SESMailProvider";
 
 import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
 import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
@@ -21,7 +23,12 @@ container.registerSingleton<IMailTemplateProvider>(
   HandlebarsMailTemplateProvider
 );
 
+const mailDriver = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider)
+};
+
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  container.resolve(EtherealMailProvider)
+  mailDriver[mailConfig.driver]
 );
