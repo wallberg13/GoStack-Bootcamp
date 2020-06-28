5384602a2de61a779c4a570ab571d02e56ebf184
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-06-27 22:46:03 -0300

    Add cache implements in ListProvidersService
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel04/03-gobarber-backend-pt03/src/modules/appointments/services/ListProvidersService.ts
+++ Nivel04/03-gobarber-backend-pt03/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,6 @@
 // import AppError from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
 
 import User from "@modules/users/infra/typeorm/entities/User";
 import IUsersRepository from "@modules/users/repositories/IUsersRepository";
@@ -13,13 +14,25 @@ class ListProvidersService {
   constructor(
     // Dizendo que o carinha que estou usando aqui foi injetado de algum lugar.
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject("CacheProvider")
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id
+      });
+
+      // ":" no redis é como se fosse um nível a mais de cache.
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
 
     return users;
   }
--- Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ Nivel04/03-gobarber-backend-pt03/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -9,15 +9,17 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
-  public async save(key: string, value: string): Promise<void> {
+  public async save(key: string, value: any): Promise<void> {
     this.client.set(key, JSON.stringify(value));
   }
 
-  public async recover(key: string): Promise<string | null> {
+  public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key);
 
     return data ? JSON.parse(data) : null;
   }
 
-  public async invalidate(key: string): Promise<void> {}
+  public async invalidate(key: string): Promise<void> {
+    console.log(key);
+  }
 }
