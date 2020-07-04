f0f554fe1f28ab784e58e6efb897d9398f65201a
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-07-04 00:45:27 -0300

    Create ResetPassword page
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel05/02-gobarber-web-pt02/src/routes/index.tsx
+++ Nivel05/02-gobarber-web-pt02/src/routes/index.tsx
@@ -6,6 +6,7 @@ import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
 import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
 
 /**
  * A autenticação das rotas devem ser verificadas no arquivo de rotas,
@@ -24,6 +25,7 @@ const Router: React.FC = () => {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
       <Route path="/forgot-password" exact component={ForgotPassword} />
+      <Route path="/reset-password" exact component={ResetPassword} />
       <Route path="/Dashboard" exact component={Dashboard} isPrivate />
     </Switch>
   );
