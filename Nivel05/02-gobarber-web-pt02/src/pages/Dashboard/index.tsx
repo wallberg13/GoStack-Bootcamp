c90b87c46fdba6b0bfea68a540d412d54eb2c8f0
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-07-05 15:21:09 -0300

    Add Next Appointments in Dashboard
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/index.tsx
+++ Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Container, Header, HeaderContent, Profile } from "./styles";
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar
+} from "./styles";
 
 import logoImg from "../../assets/logo.svg";
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiClock } from "react-icons/fi";
 import { useAuth } from "../../hooks/auth";
 
 const Dashboard: React.FC = () => {
@@ -27,6 +36,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
+                alt="Wall Berg Morais"
+              />
+
+              <strong>Wall Berg Morais</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
