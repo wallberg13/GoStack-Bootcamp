85b64e5d46e4ac85916e7a7d88b5d84979d4a88e
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-07-10 00:37:54 -0300

    Add list of appointments in Dashboard page
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/index.tsx
+++ Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/index.tsx
@@ -21,6 +21,7 @@ import {
 import logoImg from "../../assets/logo.svg";
 import { useAuth } from "../../hooks/auth";
 import api from "../../services/api";
+import { parseISO } from "date-fns/esm";
 
 interface MonthAvailabilityItem {
   day: number;
@@ -30,6 +31,7 @@ interface MonthAvailabilityItem {
 interface Appointment {
   id: string;
   date: string;
+  hourFormatted: string;
   user: {
     name: string;
     avatar_url: string;
@@ -82,7 +84,7 @@ const Dashboard: React.FC = () => {
   // Carregando os Appointments
   useEffect(() => {
     api
-      .get(`/appointments/me`, {
+      .get<Appointment[]>(`/appointments/me`, {
         params: {
           year: selectedDate.getFullYear(),
           month: selectedDate.getMonth() + 1,
@@ -90,8 +92,14 @@ const Dashboard: React.FC = () => {
         }
       })
       .then((response) => {
-        console.log(response.data);
-        setAppointments(response.data);
+        const appointmentsFormatted = response.data.map((appointment) => {
+          return {
+            ...appointment,
+            hourFormatted: format(parseISO(appointment.date), "HH:mm")
+          };
+        });
+
+        setAppointments(appointmentsFormatted);
       });
   }, [selectedDate, user.id]);
 
@@ -117,6 +125,18 @@ const Dashboard: React.FC = () => {
   const selectedWeekDay = useMemo(() => {
     return format(selectedDate, "cccc", { locale: ptBR });
   }, [selectedDate]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter((appointment) => {
+      return parseISO(appointment.date).getHours() < 12;
+    });
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter((appointment) => {
+      return parseISO(appointment.date).getHours() >= 12;
+    });
+  }, [appointments]);
 
   return (
     <Container>
@@ -164,76 +184,40 @@ const Dashboard: React.FC = () => {
           <Section>
             <strong>Manhã</strong>
 
-            <Appointment>
-              <span>
-                <FiClock /> 8:00
-              </span>
+            {morningAppointments.map((appointment) => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock /> {appointment.hourFormatted}
+                </span>
 
-              <div>
-                <img
-                  src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-                  alt="Wall Berg Morais"
-                />
-                <strong>Wall Berg Morais</strong>
-              </div>
-            </Appointment>
-
-            <Appointment>
-              <span>
-                <FiClock /> 8:00
-              </span>
-
-              <div>
-                <img
-                  src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-                  alt="Wall Berg Morais"
-                />
-                <strong>Wall Berg Morais</strong>
-              </div>
-            </Appointment>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
 
           <Section>
             <strong>Tarde</strong>
-            <Appointment>
-              <span>
-                <FiClock /> 8:00
-              </span>
+            {afternoonAppointments.map((appointment) => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock /> {appointment.hourFormatted}
+                </span>
 
-              <div>
-                <img
-                  src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-                  alt="Wall Berg Morais"
-                />
-                <strong>Wall Berg Morais</strong>
-              </div>
-            </Appointment>
-            <Appointment>
-              <span>
-                <FiClock /> 8:00
-              </span>
-
-              <div>
-                <img
-                  src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-                  alt="Wall Berg Morais"
-                />
-                <strong>Wall Berg Morais</strong>
-              </div>
-            </Appointment>
-            <Appointment>
-              <span>
-                <FiClock /> 8:00
-              </span>
-
-              <div>
-                <img
-                  src="https://avatars1.githubusercontent.com/u/8339296?s=400&u=799817970db70debe9c79d63fa4ef04f8ffaafc7&v=4"
-                  alt="Wall Berg Morais"
-                />
-                <strong>Wall Berg Morais</strong>
-              </div>
-            </Appointment>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
         </Schedule>
 
--- Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/styles.ts
+++ Nivel05/02-gobarber-web-pt02/src/pages/Dashboard/styles.ts
@@ -175,7 +175,7 @@ export const Appointment = styled.div`
     display: flex;
     align-items: center;
     color: #f4ede8;
-
+    width: 64px;
     svg {
       color: #ff9000;
       margin-right: 8px;
