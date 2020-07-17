2678309204a111f2404d4d92413f3ee15fa883a0
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-07-16 23:34:23 -0300

    Add AppointmentCreate, CreateAppointments and Profile page
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

---
+++ Nivel05/03-gobarber-mobile-pt02/src/pages/CreateAppointments/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const CreateAppointments: React.FC = () => {
+  return <View />;
+};
+
+export default CreateAppointments;
--- Nivel05/03-gobarber-mobile-pt02/src/routes/app.routes.tsx
+++ Nivel05/03-gobarber-mobile-pt02/src/routes/app.routes.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
+import CreateAppointments from "../pages/CreateAppointments";
+import AppointmentCreated from "../pages/AppointmentCreated";
 
 // Rotas para quando o usuário não está autenticado
 
@@ -23,6 +26,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointments} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
