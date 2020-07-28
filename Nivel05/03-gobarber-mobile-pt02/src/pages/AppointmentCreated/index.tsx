1daeeb6b11da729f18d096315954e3be85d42c92
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2020-07-28 00:59:53 -0300

    Finishing AppointmentCreated Screen
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Nivel05/03-gobarber-mobile-pt02/src/pages/AppointmentCreated/index.tsx
+++ Nivel05/03-gobarber-mobile-pt02/src/pages/AppointmentCreated/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import Icon from "react-native-vector-icons/Feather";
 import {
   Container,
@@ -7,10 +7,19 @@ import {
   OkButton,
   OkButtonText,
 } from "./styles";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() => {
     reset({
@@ -20,12 +29,25 @@ const AppointmentCreated: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    const dateFormatted = format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR }
+    );
+
+    let upperFirst =
+      dateFormatted[0].toUpperCase() + dateFormatted.substring(1);
+
+    return upperFirst;
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
